6ec4260880d4560f8cf9df9d5e07d12b8ad51fd1
Your Name <segunakowonjo@yahoo.com>
2017-11-09 13:37:44 -0500

    Easy to do

---
+++ loginpage.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	<title>HTML Forms</title>
+	<link rel="stylesheet" href="signup.css">
+</head>
+<body>
+
+	<h1 style="color: blue">Loginpage Page</h1>
+	<form class="form-horizontal" action="./successful.php" method="POST" target="_blank">
+E-mail address: <input type="text" name="email-address"><br><br>
+Password: <input type="text" name="password"><br></br>
+<button type="submit" class="signupbtn">Loginpage Up</button>
+       <button type="button"  class="cancelbtn">Cancel</button>
+
+
+	</form>
+</body>
+</html>
